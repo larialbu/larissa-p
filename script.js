298d14d75b31b4d269cfbb463eb06d2f92b0c7db
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2023-03-05 20:25:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
      });
 
      var typed = new Typed(".typing", {
-         strings:["Desenvolvedora", "Designer", "Dedicada", "Criativa"],
+         strings:["Desenvolvedora", "Designer", "Social Media", "Expressionista Gráfica"],
          typeSpeed:100,
          backSpeed:60,
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["Desenvolvedora", "Designer", "Dedicada", "Criativa"],
+        strings:["Desenvolvedora", "Designer", "Social Media", "Expressionista Gráfica"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
